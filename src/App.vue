d152b2798b4c22bb0ac35d7b55efd4a8f2e13e4f
Sami Kibachi <samikibachi@gmail.com>
2024-01-12 22:10:49 +0100

    log in to metamask button missing errors and CSS

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="grid place-items-center h-96">
+      <div v-if="!isLoggedIn">
+          <button
+          @click="connectWallet"
+          v-if="isMetamaskSupported"
+          class="px-4 py-2 rounded bg-gradient-to-r from-orange-500 to-orange-600 text-gray-50 font-semibold"
+          >
+          metamask login
+          </button>
+          <Title v-else>Please Install Metamask Extension</Title>
+
+      </div>
+
+      <div v-else>
+          {{computedAdress}}
+      </div>
+  </div>
+
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
+import { computed } from "vue";
+import { onMounted, ref, defineAsyncComponent } from "@vue/runtime-core";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const isLoggedIn = ref(false);
+const adress = ref("")
+const isMetamaskSupported = ref(false);
+onMounted(() => {
+  isMetamaskSupported.value = typeof (window as any).ethereum !== "undefined";
 });
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+async function connectWallet() {
+  const accounts = await (window as any).ethereum.request({ method: "eth_requestAccounts" });
+    adress.value = accounts[0];  
 }
-</style>
+
+const computedAdress = computed(() => adress.value.substring(0, 4) + "....");
+</script>
