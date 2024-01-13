a9d52c78563e970896455e4ca466a567f373f6f8
Sami Kibachi <samikibachi@gmail.com>
2024-01-13 12:57:47 +0100

    component created for metamask login

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,13 @@
 <template>
-  <div class="grid place-items-center h-96">
-      <div v-if="!isLoggedIn">
-          <button
-          @click="connectWallet"
-          v-if="isMetamaskInstalled"
-          class="px-4 py-2 rounded bg-gradient-to-r from-blue-500 to-blue-600 text-gray-50 font-semibold"
-          >
-          Metamask Login
-          </button>
-          <div v-else class="text-center text-black bg-red-500 p-4 rounded-md shadow-lg max-w-md mx-auto mt-4 font-bold">
-            Please Install Metamask Extension
-          </div>
-
-      </div>
-
-      <div v-else>
-          {{address}}
-      </div>
-  </div>
-
+  <Login />
 </template>
 
-<script lang="ts" setup>
-import { computed, onMounted, ref } from "vue";
+<script lang="ts">
+import Login from './components/Login.vue';
 
-const isLoggedIn = ref(false);
-const address = ref("");
-const isMetamaskInstalled = ref(false);
-
-onMounted(async () => {
-  isMetamaskInstalled.value = typeof window.ethereum !== 'undefined';
-});
-
-const connectWallet = async () => {
-  if (isMetamaskInstalled.value) {
-    try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      address.value = accounts[0];
-      isLoggedIn.value = true;
-    } catch (err) {
-      console.error("Failed to connect to Metamask", err);
-    }
+export default {
+  components: {
+    Login
   }
-};
+}
 </script>
