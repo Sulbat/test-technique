f17fd51984d12c2c1ce2d3e9c17df695368c0fc6
Sami Kibachi <samikibachi@gmail.com>
2024-01-13 14:51:19 +0100

    creation of authentification.vue to get message

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <Authentication />
   <Login />
 </template>
 
 <script lang="ts">
+import Authentication from './components/Authentification.vue';
 import Login from './components/Login.vue';
 
 export default {
   components: {
+    Authentication,
     Login
   }
 }
