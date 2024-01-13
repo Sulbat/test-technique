6a382d221ed8a8e94894fbcf01f9eed0367aeaa2
Sami Kibachi <samikibachi@gmail.com>
2024-01-13 17:46:24 +0100

    sign message

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <Authentication />
   <Login />
 </template>
 
 <script lang="ts">
-import Authentication from './components/Authentification.vue';
 import Login from './components/Login.vue';
 
 export default {
   components: {
-    Authentication,
     Login
   }
 }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,46 +1,78 @@
 <template>
-    <div class="grid place-items-center h-96">
-        <div v-if="!isLoggedIn">
-            <button
-            @click="connectWallet"
-            v-if="isMetamaskInstalled"
-            class="px-4 py-2 rounded bg-gradient-to-r from-blue-500 to-blue-600 text-gray-50 font-semibold"
-            >
-            Metamask Login
-            </button>
-            <div v-else class="text-center text-black bg-red-500 p-4 rounded-md shadow-lg max-w-md mx-auto mt-4 font-bold">
-              Please Install Metamask Extension
-            </div>
-  
-        </div>
-  
-        <div v-else>
-            {{address}}
-        </div>
+  <div class="grid place-items-center h-96">
+    <div v-if="!isLoggedIn">
+      <button
+        @click="connectWallet"
+        v-if="isMetamaskInstalled"
+        class="px-4 py-2 rounded bg-gradient-to-r from-blue-500 to-blue-600 text-gray-50 font-semibold"
+      >
+        Metamask Login
+      </button>
+      <div
+        v-else
+        class="text-center text-black bg-red-500 p-4 rounded-md shadow-lg max-w-md mx-auto mt-4 font-bold"
+      >
+        Please Install Metamask Extension
+      </div>
     </div>
-  
-  </template>
-  
-  <script lang="ts" setup>
-  import { computed, onMounted, ref } from "vue";
-  
-  const isLoggedIn = ref(false);
-  const address = ref("");
-  const isMetamaskInstalled = ref(false);
-  
-  onMounted(async () => {
-    isMetamaskInstalled.value = typeof window.ethereum !== 'undefined';
-  });
-  
-  const connectWallet = async () => {
-    if (isMetamaskInstalled.value) {
-      try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        address.value = accounts[0];
-        isLoggedIn.value = true;
-      } catch (err) {
-        console.error("Failed to connect to Metamask", err);
-      }
+
+    <div v-else>
+      {{ address }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, onMounted, ref } from "vue";
+import axios from "axios";
+import { ethers } from 'ethers';
+
+const isLoggedIn = ref(false);
+const address = ref("");
+const isMetamaskInstalled = ref(false);
+
+onMounted(async () => {
+  isMetamaskInstalled.value = typeof window.ethereum !== "undefined";
+});
+
+const connectWallet = async () => {
+  if (isMetamaskInstalled.value) {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      address.value = accounts[0];
+      isLoggedIn.value = true;
+
+      // Log the address after it has been updated
+      signWallet()
+    } catch (err) {
+      console.error("Failed to connect to Metamask", err);
     }
-  };
-  </script>
+  }
+};
+const signWallet = async () => {
+  try {
+    const addressWallet = address.value;
+    const apiUrl = "https://backend-quiz-dot-tools-303211.nw.r.appspot.com/auth/message";
+    const response = await axios.get(`${apiUrl}/${addressWallet}`, {
+      headers: {
+        accept: "application/json",
+      },
+    });
+    const message = response.data.message;
+    const sign = await window.ethereum.request({
+        method: "personal_sign",
+        params: [message, addressWallet],
+      });
+    
+      console.log(sign);
+    
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+</script>
