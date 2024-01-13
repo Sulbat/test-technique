8335364625b47a79b6f6e4b174e4e47f33b19abc
Sami Kibachi <samikibachi@gmail.com>
2024-01-13 12:48:56 +0100

    no metamask error message

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
       <div v-if="!isLoggedIn">
           <button
           @click="connectWallet"
-          v-if="isMetamaskSupported"
+          v-if="isMetamaskInstalled"
           class="px-4 py-2 rounded bg-gradient-to-r from-blue-500 to-blue-600 text-gray-50 font-semibold"
           >
           Metamask Login
           </button>
-          <Title v-else>Please Install Metamask Extension</Title>
+          <div v-else class="text-center text-black bg-red-500 p-4 rounded-md shadow-lg max-w-md mx-auto mt-4 font-bold">
+            Please Install Metamask Extension
+          </div>
 
       </div>
 
       <div v-else>
-          {{computedAdress}}
+          {{address}}
       </div>
   </div>
 
@@ -23,22 +25,22 @@
 import { computed, onMounted, ref } from "vue";
 
 const isLoggedIn = ref(false);
-const adress = ref("");
-const isMetamaskSupported = ref(false);
+const address = ref("");
+const isMetamaskInstalled = ref(false);
 
-onMounted(() => {
-  isMetamaskSupported.value = typeof (window as any).ethereum !== "undefined";
+onMounted(async () => {
+  isMetamaskInstalled.value = typeof window.ethereum !== 'undefined';
 });
 
-async function connectWallet() {
-  try {
-    const accounts = await (window as any).ethereum.request({ method: "eth_requestAccounts" });
-    adress.value = accounts[0];
-    isLoggedIn.value = true;
-  } catch (error) {
-    console.error("Erreur lors de la connexion au portefeuille : ", error);
+const connectWallet = async () => {
+  if (isMetamaskInstalled.value) {
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      address.value = accounts[0];
+      isLoggedIn.value = true;
+    } catch (err) {
+      console.error("Failed to connect to Metamask", err);
+    }
   }
-}
-
-const computedAdress = computed(() => adress.value ? adress.value.substring(0, 4) + "...." : '');
+};
 </script>
